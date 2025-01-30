2e9b52a60cca11e2a72fb14e87a618470c409c74
suedecrc <dev.suede@gmail.com>
2025-01-30 12:54:32 -0400

    Novas rotas e funções criadas da página de perfil do fretista.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('home/', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('perfil_fretista/', views.perfil_fretista, name='perfil_fretista'),
+    path('conversas/', views.conversas, name='conversas'),
+    path('enderecos/', views.enderecos, name='enderecos'),
+    path('dados-da-conta/', views.dados_da_conta, name='dados_da_conta'),
+    path('meus-agendamentos/', views.meus_agendamentos, name='meus_agendamentos'),
+    path('configuracoes/', views.configuracoes, name='configuracoes'),
+    path('seguranca/', views.seguranca, name='seguranca'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,44 +2,50 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib import messages
-from django.contrib.auth import login as login_django, logout as logout_django
+from django.contrib.auth import login as login_django
 
-######## Pagino de login, ta dando erro essa bosta ########
+
 def login(request):
     if request.method == "GET":
         return render(request, 'usuarios/login.html')
     else:
-        email = request.POST.get('email')
+        username = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        user = authenticate(username=email, password=senha) ######## erro
+        user = authenticate(username=username, password=senha)
 
         if user:
             login_django(request, user)
             return HttpResponse('Sucesso!')
         else:
-            return HttpResponse('Erro')
+            return HttpResponse('Credenciais inválidas!')
 
-
-######## Pagina de cadastro ########
 def cadastro(request):
-    if request.method == "GET":
-        return render(request, 'usuarios/cadastro.html')
-    else:
-        nome = request.POST.get('nome')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
+    return render(request, 'usuarios/cadastro.html')
 
-        user = User.objects.create_user(username=nome, email=email, password=senha)
-
-        if user:
-            login_django(request, user)
-            return HttpResponse('Cadastrado com sucesso!')
-        else:
-            return HttpResponse('Conta já existente!')
-    
-
-######## pagina inicial ########
 def home(request):
     return render(request, 'usuarios/home.html')
+
+def perfil_fretista(request):
+    return render(request, 'usuarios/perfil_fretista.html')
+
+def conversas(request):
+    return render(request, 'usuarios/conversas.html')
+
+def enderecos(request):
+    return render(request, 'usuarios/enderecos.html')
+
+def dados_da_conta(request):
+    return render(request, 'usuarios/dados_da_conta.html')
+
+def meus_agendamentos(request):
+    return render(request, 'usuarios/meus_agendamentos.html')
+
+def configuracoes(request):
+    return render(request, 'usuarios/configuracoes.html')
+
+def seguranca(request):
+    return render(request, 'usuarios/seguranca.html')
+
+def logout_view(request):
+    return render(request, 'usuarios/logout.html')
