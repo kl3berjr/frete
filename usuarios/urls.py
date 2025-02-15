4aa21b5946c00e302fd1f483bc53dfd3f76694f3
kl3berjr <kleberjunior147@gmail.com>
2025-02-15 09:32:30 -0400

    up19

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,11 +6,4 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('fretista/', views.fretista, name='fretista'),
-    path('conversas/', views.conversas, name='conversas'),
-    path('enderecos/', views.enderecos, name='enderecos'),
-    path('dados-da-conta/', views.dados_da_conta, name='dados_da_conta'),
-    path('meus-agendamentos/', views.meus_agendamentos, name='meus_agendamentos'),
-    path('configuracoes/', views.configuracoes, name='configuracoes'),
-    path('seguranca/', views.seguranca, name='seguranca'),
-    path('logout/', views.logout_view, name='logout'),
 ]
